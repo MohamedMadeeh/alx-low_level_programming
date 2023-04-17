e98ebe57f203be1934327279ed6d032fd29ada4a
Mohamed Madeeh <mohamedmadeeh1106@gmail.com>
2023-04-17 04:41:45 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,14 +18,14 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(struct dog *dd, char *name, float age, char *owner);
+void print_dog(struct dog *dd);
 
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
-void free_dog(dog_t *d);
+void free_dog(dog_t *dd);
 
 
 
